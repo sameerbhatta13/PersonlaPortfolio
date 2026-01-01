a669451f413d5aa1f1b3304514cd5e99ec04f31d
sameer bhatta <sameerbhatta37@gmail.com>
2026-01-01 12:15:38 +0545

    update the name

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -6,7 +6,7 @@ const HomePage = () => {
             <img src={heroImage} alt="" className='mx-auto mb-8 w-48 h-48 rounded-full object-cover transform transition-transform duration-300 hover:scale-105' />
             <h1 className='text-4xl font-bold'> I'm {""}
                 <span className='text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500'> Sameer Bhatta </span>
-                MERN-stack Developer
+                Full-stack Javascript Developer
             </h1>
             <p className='mt-4 text-lg text-gray-300 '>
                 i am specialize in building modern and responsive applications
